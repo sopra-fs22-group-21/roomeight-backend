7cc94be7c99c144e68b39928dbd4a4bbba782455
Christoph_Bachmann+1-at-748934571076 <christoph.bachmann@oerlikon.com>
2022-03-31 18:30:59 +0200

    workflow change4

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ import {Tag} from "./data-model/Tag";
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const test2 = functions.https.onRequest((request, response) => {
+export const test = functions.https.onRequest((request, response) => {
   let tag = new Tag("test", "test");
   tag.test();
   response.send("Hello2");
