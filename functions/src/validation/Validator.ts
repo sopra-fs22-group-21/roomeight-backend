16b3c746a7274cd2c52f48740808efb1e3973971
lars-boesch <lars.boesch@gmail.com>
2022-04-03 11:53:01 +0200

    updated validator

--- functions/src/validation/Validator.ts
+++ functions/src/validation/Validator.ts
@@ -5,8 +5,10 @@ export class Validator {
         let report = new ValidationReport();
 
         let mandatoryFields = ["FirstName", "LastName", "Birthday", "EmailAddress", "PhoneNumber", "Password"];
-        let optionalFields = ["Description", "Biography", "Tags", "PictureReference", "Matches", "OnlineStatus",
-                              "Gender", "IsSearchingRoom", "IsAdvertisingRoom", "MoveInDate", "MoveOutDate"]
+        let optionalFields = ["Description", "Biography", "Tags", "PictureReference", "Matches",
+                              "Gender", "IsSearchingRoom", "IsAdvertisingRoom", "MoveInDate", "MoveOutDate"];
+        
+        let allowedGenders = ["MALE", "FEMALE", "OTHERS"];
         for (let i in mandatoryFields) {
             if(!user_json_body.hasOwnProperty(mandatoryFields[i])) {
                 report.setErrors("JSON object does not contain required field: " + mandatoryFields[i]);
@@ -32,13 +34,13 @@ export class Validator {
                     }
                     break;
                 case "Birthday":
-                    if (!this.validateBirthday(user_json_body[key])) {
-                        report.setErrors("invalid Birthday");
+                    if (!this.validateDate(user_json_body[key])) {
+                        report.setErrors("invalid Birthday, Expected Format: 1999-06-22");
                     }
                     break;
                 case "EmailAddress":
                     if (!this.validateEmail(user_json_body[key])) {
-                        report.setErrors("invalid EmailAddress, Expected Format: 1999-06-22");
+                        report.setErrors("invalid EmailAddress");
                     }
                     break;
                 case "PhoneNumber":
@@ -46,14 +48,69 @@ export class Validator {
                         report.setErrors("invalid PhoneNumber");
                     }
                     break;
+                case "Description":
+                    if (!this.validateString(user_json_body[key])) {
+                        report.setErrors("invalid Description");
+                    }
+                    break;
+                case "Biography":
+                    if (!this.validateString(user_json_body[key])) {
+                        report.setErrors("invalid Description");
+                    }
+                    break;
+                case "Tags":
+                    if (!this.validateTags(user_json_body[key])) {
+                        report.setErrors("invalid Tags");
+                    }
+                    break;
+                case "PictureReference":
+                    if (!this.validateString(user_json_body[key])) {
+                        report.setErrors("invalid PictureReference");
+                    }
+                    break;
+                case "Matches":
+                    if (!this.validateMatches(user_json_body[key])) {
+                        report.setErrors("invalid Matches");
+                    }
+                    break;
+                case "Gender":
+                    if (!this.validateGender(user_json_body[key], allowedGenders)) {
+                        report.setErrors("invalid Gender, must be MALE/FEMALE or OTHERS");
+                    }
+                    break;
+                case "IsSearchingRoom":
+                    if (!this.validateBoolean(user_json_body[key])) {
+                        report.setErrors("invalid IsSearchingRoom, has to be true or false (string)");
+                    }
+                    break;
+                case "IsAdvertisingRoom":
+                    if (!this.validateBoolean(user_json_body[key])) {
+                        report.setErrors("invalid IsAdvertisingRoom, has to be true or false (string)");
+                    }
+                    break;
+                case "MoveInDate":
+                    if (!this.validateDate(user_json_body[key])) {
+                        report.setErrors("invalid MoveInDate, Expected Format: 1999-06-22");
+                    }
+                    break;
+                case "MoveOutDate":
+                    if (!this.validateDate(user_json_body[key])) {
+                        report.setErrors("invalid MoveOutDate, Expected Format: 1999-06-22");
+                    }
+                    break;
                 default:
-                    // Todo validate optional fields
                     if (optionalFields.indexOf(key) == -1) {
                         report.setErrors("Unknown Field: " + key);
                     }
             }
         }
         return report;
+    }
+    private static validateGender(gender: string, allowedGenders: string[]): boolean {
+        return (allowedGenders.includes(gender));
+    }
+    private static validateBoolean(bool: string): boolean {
+        return (bool === "true" || bool === "false");
     }
     private static validatePhone(phone: string): boolean {
         const regex = new RegExp('/(\b(0041|0)|\B\+41)(\s?\(0\))?(\s)?[1-9]{2}(\s)?[0-9]{3}(\s)?[0-9]{2}(\s)?[0-9]{2}\b/');
@@ -63,15 +120,23 @@ export class Validator {
         let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
         return regex.test(mail);
     }
-    private static validateBirthday(birthday: string): boolean {
-        return (!isNaN(Date.parse(birthday)));
+    private static validateDate(date: string): boolean {
+        return (!isNaN(Date.parse(date)));
+    }
+    private static validateString(name: string): boolean {
+        return (name.length > 0 && name.length < 300);
     }
     private static validateName(name: string): boolean {
-        return (name.length > 1 && name.length < 50);
+        return (name.length > 0 && name.length < 50);
     }
-
     private static validatePassword(password: string) {
         return (password.length > 5 && password.length < 50);
+    }
+    private static validateTags(name: string): boolean {
+        return (name.length > 0 && name.length < 100000);
+    }
+    private static validateMatches(name: string): boolean {
+        return (name.length > 0 && name.length < 100000);
     }
 }
 
