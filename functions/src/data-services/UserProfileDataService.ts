5c8d01fa7ff7f7d71fc1ad353203acbb88dcc04e
Christoph_Bachmann+1-at-748934571076 <christoph.bachmann@oerlikon.com>
2022-04-03 11:28:00 +0200

    improved error handling

--- functions/src/data-services/UserProfileDataService.ts
+++ functions/src/data-services/UserProfileDataService.ts
@@ -1,9 +1,7 @@
 import {Repository} from "../repository/Repository";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, deleteUser } from "firebase/auth";
 import {UserProfile} from "../data-model/UserProfile";
 import {Validator} from "../validation/Validator";
-import {FirestoreError} from "../exceptions/FirestoreError";
-import {AuthError} from "../exceptions/AuthError";
 
 export class UserProfileDataService {
 
@@ -16,7 +14,6 @@ export class UserProfileDataService {
         // Validate user which should be added
         const validation_results = Validator.validateUser(body);
 
-        // Todo improve error handling
         if (!validation_results.hasErrors) {
             // Precede if validation found no errors
             let user_to_add = new UserProfile(body.FirstName, body.LastName, body.Description, body.Biography, body.Tags,
@@ -25,33 +22,24 @@ export class UserProfileDataService {
                 body.PhoneNumber, body.Gender, body.IsSearchingRoom, body.IsAdvertisingRoom)
 
 
-            return repository.addUserProfile(user_to_add.toJson()).then((response) => {
-                // As soon as the user object is posted into the database precede with auth user profile creation
-                return createUserWithEmailAndPassword(auth, body.EmailAddress, body.Password)
-                    .then((userCredential) => {
-                        return user_to_add.toJson();
-                    })
-                    .catch((error) => {
-                        const errorCode = error.code;
-                        const errorMessage = error.message;
-                        console.log(errorCode);
-                        console.log(errorMessage);
-
-                        repository.deleteUserProfile(user_to_add.emailAddress).then(
-                            (response) => {
-                                console.log("Response from DB cleanup due to auth user creation failure: " + response);
-                            }
-                        );
-                        throw new FirestoreError(errorMessage);
-                    });
-                })
-                .catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    console.log(errorCode);
-                    console.log(errorMessage)
-                    throw new AuthError(errorMessage);
+            // As soon as the user object is posted into the database precede with auth user profile creation
+            return createUserWithEmailAndPassword(auth, body.EmailAddress, body.Password)
+                .then((userCredential) => {
+                    return repository.addUserProfile(user_to_add.toJson())
+                        .then((response) => {
+                            return user_to_add.toJson();
+                        })
+                        .catch((e) => {
+                            deleteUser(userCredential.user)
+                                .then(() => {
+                                        throw new Error("Could not post user object to firestore " +
+                                                        "but could also not delete already signed up auth user");
+                                    }
+                                );
+                            throw new Error("Error: Could not post user due to: " + e.message)
+                        });
                 });
+
         } else {
             // Throw value error with list of errors which were found if validation failed
             throw new Error(validation_results.errors.toString());
