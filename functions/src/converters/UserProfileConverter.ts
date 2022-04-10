bcb6aec2ca4ac8a082b64455004f0aa6657d5e77
lars-boesch <lars.boesch@gmail.com>
2022-04-10 10:26:57 +0200

    bugfix in userconverter after json adjustment

--- functions/src/converters/UserProfileConverter.ts
+++ functions/src/converters/UserProfileConverter.ts
@@ -15,31 +15,31 @@ export class UserProfileConverter {
 
         // Check if optional fields are in the json body
         if (json_body.hasOwnProperty("description")) {
-            user.description = json_body.Description;
+            user.description = json_body.description;
         }
         if (json_body.hasOwnProperty("biography")) {
-            user.biography = json_body.Biography;
+            user.biography = json_body.biography;
         }
         if (json_body.hasOwnProperty("tags")) {
-            user.tags = json_body.Tags.split(",");
+            user.tags = json_body.tags.split(",");
         }
         if (json_body.hasOwnProperty("description")) {
-            user.description = json_body.Description;
+            user.description = json_body.description;
         }
         if (json_body.hasOwnProperty("pictureReference")) {
-            user.pictureReference = json_body.PictureReference;
+            user.pictureReference = json_body.pictureReference;
         }
         if (json_body.hasOwnProperty("gender")) {
-            user.gender = json_body.Gender;
+            user.gender = json_body.gender;
         }
         if (json_body.hasOwnProperty("moveInDate")) {
-            user.moveInDate = new Date(Date.parse(json_body.MoveInDate));
+            user.moveInDate = new Date(Date.parse(json_body.moveInDate));
         }
         if (json_body.hasOwnProperty("moveOutDate")) {
-            user.moveOutDate = new Date(Date.parse(json_body.MoveOutDate));
+            user.moveOutDate = new Date(Date.parse(json_body.moveOutDate));
         }
         if (json_body.hasOwnProperty("isSearchingRoom")) {
-            user.isSearchingRoom = json_body.IsSearchingRoom == "True";
+            user.isSearchingRoom = json_body.isSearchingRoom == "True";
         }
         if (json_body.hasOwnProperty("isAdvertisingRoom")) {
             user.isAdvertisingRoom = json_body.isAdvertisingRoom == "True";
