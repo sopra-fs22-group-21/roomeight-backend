f8a02638bcbb6433ed43bf49e98e6b069d0ee993
Christoph_Bachmann+1-at-748934571076 <christoph.bachmann@oerlikon.com>
2022-04-15 00:49:15 +0200

    small change

--- functions/src/main/data-services/UserProfileDataService.ts
+++ functions/src/main/data-services/UserProfileDataService.ts
@@ -6,9 +6,9 @@ import {UserProfileConverter} from "../converters/UserProfileConverter";
 import {initializeApp} from "firebase/app";
 import {config} from "../../../firebase_config";
 // Prod import for admin auth
-import {getAuth as adminGetAuth} from "firebase-admin/auth";
+// import {getAuth as adminGetAuth} from "firebase-admin/auth";
 // Testing import for admin auth
-// import {getAuth as adminGetAuth} from "firebase-admin/lib/auth";
+import {getAuth as adminGetAuth} from "firebase-admin/lib/auth";
 
 export class UserProfileDataService {
 
