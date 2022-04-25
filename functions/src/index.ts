a599c3dd36ec83c875fc886418be6247e0d6e645
lars-boesch <lars.boesch@gmail.com>
2022-04-25 17:45:30 +0200

    update-endpoint for flat, closes #66, closes #69

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -240,46 +240,41 @@ flatprofile_app.post('/roommate/:mate_id', async (req, res) => {
 });
 
 // Update Flat
-// flatprofile_app.patch('/:profileId', async (req, res) => {
-//     functions.logger.debug("Started Flat Patch Request", {structuredData: true});
-//     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-//         // Get Profile Id and Token from request
-//         const idToken = req.headers.authorization.split('Bearer ')[1]
-//         const profile_id = sanitizeHtml(req.params.profileId);
-//
-//         getAuth()
-//             .verifyIdToken(idToken)
-//             .then((decodedToken) => {
-//                 functions.logger.debug("Started Flat Post Request", {structuredData: true});
-//                 if (uid == profile_id) {
-//                     // If uid of token matches the profileId continue with request processing
-//                     userProfileDataService.updateUser(req.body, profile_id)
-//                         .then(
-//                             (data_service_response) => {
-//                                 res.set('Access-Control-Allow-Origin', '*')
-//                                 res.status(200).send(data_service_response);
-//                             }
-//                         )
-//                         .catch(
-//                             (e) => {
-//                                 // Return HTTP Code 400 if error occurred
-//                                 functions.logger.debug(e, {structuredData: true})
-//                                 res.status(400).send(e.message);
-//                             }
-//                         );
-//                 } else {
-//                     // Else return NotAuthorized-Exception
-//                     res.status(403).send("Not authorized to update the selected user!");
-//                 }
-//             })
-//             .catch((error) => {
-//                 res.status(401).send("Authorization failed: " + error);
-//             });
-//
-//     } else {
-//         res.status(401).send("Authorization failed: No authorization header present");
-//     }
-// });
+flatprofile_app.patch('/:profileId', async (req, res) => {
+    functions.logger.debug("Started Flat Patch Request", {structuredData: true});
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        // Get Profile Id and Token from request
+        const idToken = req.headers.authorization.split('Bearer ')[1]
+        const flat_id = sanitizeHtml(req.params.profileId);
+
+        getAuth()
+            .verifyIdToken(idToken)
+            .then((decodedToken) => {
+                functions.logger.debug("Started Flat Patch Request", {structuredData: true});
+                    // If uid of token matches the profileId continue with request processing
+                    return flatProfileDataService.updateFlat(req.body, flat_id, decodedToken.uid)
+                        .then(
+                            (data_service_response) => {
+                                res.set('Access-Control-Allow-Origin', '*')
+                                res.status(200).send(data_service_response);
+                            }
+                        )
+                        .catch(
+                            (e) => {
+                                // Return HTTP Code 400 if error occurred
+                                functions.logger.debug(e, {structuredData: true})
+                                res.status(400).send(e.message);
+                            }
+                        );
+            })
+            .catch((error) => {
+                res.status(401).send("Authorization failed: " + error);
+            });
+
+    } else {
+        res.status(401).send("Authorization failed: No authorization header present");
+    }
+});
 
 // Delete Flat
 flatprofile_app.delete('/:profileId', async (req, res) => {
