449644490848afa24064184e417a8d70097d0c74
Christoph_Bachmann+1-at-748934571076 <christoph.bachmann@oerlikon.com>
2022-04-28 14:52:42 +0200

    altered resolved reference appearence

--- functions/src/test/ReferenceHandling.test.ts
+++ functions/src/test/ReferenceHandling.test.ts
@@ -8,27 +8,29 @@ describe("ReferenceController Test", () => {
     // List Resolving Tests
 
     test('1 Test Resolving of Reference List containing single element', () => {
-        const expected_answer = new Array({
-            profileId: '',
-            firstName: 'Mock first_name',
-            lastName: 'Mock last_name',
-            description: '',
-            biography: '',
-            tags: [],
-            pictureReferences: [],
-            matches: [],
-            creationDate: "1970-01-01T00:00:00.000Z",
-            onlineStatus: 'ONLINE',
-            birthday: "1970-01-01T00:00:00.000Z",
-            email: 'test@test.com',
-            phoneNumber: '0795556677',
-            gender: 'NOT SET',
-            isSearchingRoom: false,
-            isAdvertisingRoom: false,
-            moveInDate: "1970-01-01T00:00:00.000Z",
-            moveOutDate:  "1970-01-01T00:00:00.000Z",
-            flatId: ''
-        });
+        const expected_answer = {
+            "": {
+                profileId: '',
+                firstName: 'Mock first_name',
+                lastName: 'Mock last_name',
+                description: '',
+                biography: '',
+                tags: [],
+                pictureReferences: [],
+                matches: [],
+                creationDate: "1970-01-01T00:00:00.000Z",
+                onlineStatus: 'ONLINE',
+                birthday: "1970-01-01T00:00:00.000Z",
+                email: 'test@test.com',
+                phoneNumber: '0795556677',
+                gender: 'NOT SET',
+                isSearchingRoom: false,
+                isAdvertisingRoom: false,
+                moveInDate: "1970-01-01T00:00:00.000Z",
+                moveOutDate:  "1970-01-01T00:00:00.000Z",
+                flatId: ''
+            }
+        }
         const reference_list = ["123"]
         const mock_user_repo = new ValidMockUserRepository();
         const resolver = new ReferenceController(mock_user_repo);
@@ -47,7 +49,7 @@ describe("ReferenceController Test", () => {
 
         return resolver.resolveProfileReferenceList(reference_list).then((response) => {
             console.log(response);
-            expect(JSON.stringify(response.result)).toEqual("[]");
+            expect(JSON.stringify(response.result)).toEqual("{}");
             expect(JSON.stringify(response.unresolvedReferences)).toEqual("[\"123\"]");
         })
     });
@@ -81,26 +83,28 @@ describe("ReferenceController Test", () => {
     // Resolve Single Reference
     test('5 Test Resolving of Single Reference', () => {
         const expected_answer = {
-            profileId: '',
-            firstName: 'Mock first_name',
-            lastName: 'Mock last_name',
-            description: '',
-            biography: '',
-            tags: [],
-            pictureReferences: [],
-            matches: [],
-            creationDate: "1970-01-01T00:00:00.000Z",
-            onlineStatus: 'ONLINE',
-            birthday: "1970-01-01T00:00:00.000Z",
-            email: 'test@test.com',
-            phoneNumber: '0795556677',
-            gender: 'NOT SET',
-            isSearchingRoom: false,
-            isAdvertisingRoom: false,
-            moveInDate: "1970-01-01T00:00:00.000Z",
-            moveOutDate:  "1970-01-01T00:00:00.000Z",
-            flatId: ''
-        };
+            "": {
+                profileId: '',
+                firstName: 'Mock first_name',
+                lastName: 'Mock last_name',
+                description: '',
+                biography: '',
+                tags: [],
+                pictureReferences: [],
+                matches: [],
+                creationDate: "1970-01-01T00:00:00.000Z",
+                onlineStatus: 'ONLINE',
+                birthday: "1970-01-01T00:00:00.000Z",
+                email: 'test@test.com',
+                phoneNumber: '0795556677',
+                gender: 'NOT SET',
+                isSearchingRoom: false,
+                isAdvertisingRoom: false,
+                moveInDate: "1970-01-01T00:00:00.000Z",
+                moveOutDate:  "1970-01-01T00:00:00.000Z",
+                flatId: ''
+            }
+        }
         const mock_user_repo = new ValidMockUserRepository();
         const resolver = new ReferenceController(mock_user_repo);
 
@@ -118,7 +122,7 @@ describe("ReferenceController Test", () => {
 
         return resolver.resolveProfileReferenceList(reference_list).then((response) => {
             console.log(response);
-            expect(JSON.stringify(response.result)).toEqual("[]");
+            expect(JSON.stringify(response.result)).toEqual("{}");
             expect(JSON.stringify(response.unresolvedReferences)).toEqual("[\"123\"]");
         })
     });
