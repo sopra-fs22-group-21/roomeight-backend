8ff07818e92f57882a244d664af4360d5c142635
flyingbeat <69470817+flyingbeat@users.noreply.github.com>
2022-05-21 14:34:49 +0200

    merge commit

---
+++ functions/src/main/repository/MockChatRepository.ts
@@ -0,0 +1,99 @@
+import {ChatInfo, MessageData, Updates} from "../../assets/Types";
+
+export class ValidMockChatRepository {
+    private database: any;
+    private chats_ref: any;
+    private memberships_ref: any;
+    private messages_ref: any;
+
+    constructor() {
+        this.database = null;
+        this.chats_ref = null;
+        this.messages_ref = null;
+        this.memberships_ref = null;
+    }
+
+    useRefs(){
+        let test: any;
+        test['one'] = this.chats_ref
+        test['two'] = this.database
+        test['three'] = this.memberships_ref
+        test['four'] = this.messages_ref
+        return test
+    }
+
+    async addMessage(chatId: string, message: MessageData) {
+        console.log('entered Mock addMessage')
+        return "message added"
+    }
+
+    async removeMessages(chatId: string) {
+        console.log('entered Mock removeMessages')
+        return "done"
+    }
+
+    async updateChatInfo(chatId: string, data: Updates | null) {
+        console.log('entered Mock updateChatInfo')
+        return "okay"
+    }
+
+    async getChatInfo(chatId: string): Promise<ChatInfo> {
+        console.log('entered Mock getChatInfo')
+        return Promise.resolve({
+            _id: 'chatId',
+            createdAt: 1,
+            members: {
+                id1: true
+            },
+            flatId: 'test',
+            userId: 'test',
+            title: {
+                forFlat: 'test',
+                forUser: 'test'
+            }
+
+        })
+    }
+
+    async getMembers(chatId: string): Promise<string[]> {
+        console.log('entered Mock getMembers')
+        return Promise.resolve([
+            'user1',
+            'user2'
+        ])
+    }
+
+    async removeMemberships(chatId: any, members: string[]) {
+        console.log('entered Mock removeMemberships')
+        return 'done';
+    }
+
+    async addMemberships(chatId: string, profileId: string, members: string[]) {
+        console.log('entered Mock addMemberships')
+        return 'done'
+    }
+
+    async getMemberships(userId: string): Promise<string[] | null> {
+        console.log('entered Mock getMemberships')
+        return Promise.resolve([
+            'chatId1',
+            'chatId2'
+        ])
+    }
+
+    async getMembershipValues(userId: string): Promise<string[] | null> {
+        console.log('entered Mock getMembershipValues')
+        if(userId.includes('hasChat')){
+            return Promise.resolve([
+                'profile1',
+                'flt$match'
+            ])
+        }
+        return Promise.resolve([
+            'profile1',
+            'flt$test'
+        ])
+    }
+
+
+}
--- functions/src/test/ChatService.test.ts
+++ functions/src/test/ChatService.test.ts
@@ -1,10 +1,116 @@
-// Unit Tests
+import {ValidMockUserRepository} from "../main/repository/MockUserRepository";
+import {ValidMockFlatRepository} from "../main/repository/MockFlatRepository";
+import {ValidMockChatRepository} from "../main/repository/MockChatRepository";
+
+jest.doMock('../main/repository/FlatRepository', ()=>{
+    return {
+        getProfileById: jest.fn().mockImplementation(
+            (profileId)=>{
+                return {
+                    name: "flatTestName",
+                    address: "AdressX",
+                    description: "descriptionX",
+                    biography: "BioX",
+                    tags: ["TAGX"],
+                    pictureReferences: ["referenceX"],
+                    moveInDate: new Date(),
+                    moveOutDate: new Date(),
+                    rent: 500,
+                    permanent: false,
+                    roomSize: 18,
+                    numberOfBaths: 1,
+                    roomMates: ["123-advertising"],
+                    likes: [],
+                    matches: ["123"],
+                    flatId: "flt$0afc1a97-2cff-4ba3-9d27-c5cad8295acb",
+                    creationDate: new Date(),
+                    addressCoordinates: {
+                        longitude: 12.34,
+                        latitude: 56.78
+                    }
+                }
+            }
+        )
+    }
+})
+
+jest.doMock('../main/repository/UserRepository', ()=>{
+    return {
+        constructor: jest.fn().mockImplementation(()=>{return null}),
+        getProfiles: jest.fn().mockImplementation(()=>{return null}),
+        getProfileById: jest.fn().mockImplementation((profileId)=>{
+            return {
+                firstName: 'Mock first_name',
+                moveInDate: new Date(),
+                likes: [],
+                phoneNumber: '0795556677',
+                flatId: '123',
+                tags: [],
+                isSearchingRoom: false,
+                isAdvertisingRoom: true,
+                biography: '',
+                profileId: '123-advertising',
+                gender: 'NOT SET',
+                pictureReferences: [],
+                birthday:  new Date(),
+                description: '',
+                lastName: 'Mock last_name',
+                onlineStatus: 'ONLINE',
+                viewed: [],
+                creationDate:  new Date(),
+                moveOutDate:  new Date(),
+                matches: [],
+                email: 'test@test.com',
+                filters: {},
+                isComplete: false
+            }
+        })
+    }
+})
+
+jest.doMock('../main/repository/ChatRepository', ()=>{
+    return {
+        getMembershipValues: jest.fn().mockImplementation(() => {
+            return [
+                'flt$test1'
+            ]
+        })
+    }
+})
+
+const { chatService } = require('../main/chat-service/chatService')
+
+
+/**
+ * userProfiles with different attributes:
+ * 'didMatch' - user has a match with other profile -> 'flt$match' createChat('flt$match', didMatch-...)
+ * 'didNotMatch' - contrary
+ * 'hasChat' - already has a chat with other profile
+ * 'doesNotHaveChat' - contrary
+ * 'FLAT' - user is part of a flat
+ * 'USER' - is searching
+ *
+ */
 
 describe("ChatService - chat operations", () => {
-
+    let chatservice: typeof chatService;
+    beforeEach(()=>{
+        chatservice = new chatService(new ValidMockUserRepository(), new ValidMockChatRepository(), new ValidMockFlatRepository())
+    })
     // Post Chat Tests
+    test('1 Test invalid did not match request', async () => {
+        try {
+            await chatservice.createChat('flt$match', 'didNotMatch')
+        } catch (e: any) {
+            expect(e.message).toEqual('User has not yet matched with this profile - cannot create chat')
+        }
+    });
 
-    test('1 Test Valid Create Chat Request', () => {
-
+    test('1 Test invalid already has chat request', async () => {
+        try {
+            await chatservice.createChat('flt$match', 'didMatch-hasChat')
+        } catch (e: any) {
+            expect(e.message).toEqual('User already has a chat with this profile')
+        }
     });
 });
